556b0a1e62e7c6aeb775e7e0691458ca09fdc0e0
volkan <volkandogru11@gmail.com>
2022-09-01 12:43:51 +0300

    random quotes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./style.css";
+import { FaQuoteLeft ,FaQuoteRight } from "react-icons/fa";
+import { AiOutlineMinus } from "react-icons/ai";
 
 function App() {
+  const [quote, setQuote] = useState("");
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((cevap) => cevap.json())
+      .then((veri) => {
+        let randNum = Math.floor(Math.random() * veri.length);
+        setQuote(veri[randNum]);
+      });
+  };
+
+  useEffect(() =>{
+    getQuote()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="quote">
+        <div className="quote-container">
+          <div className="quote-text"> <FaQuoteLeft /> {quote.text} <FaQuoteRight />
+          </div>
+          <div className="quote-author">
+            <AiOutlineMinus className="line"/> <b>{quote.author}</b> 
+          </div>
+          <button onClick={getQuote}>Get Quote</button>
+        </div>
+      </div>
+    </>
   );
 }
 
